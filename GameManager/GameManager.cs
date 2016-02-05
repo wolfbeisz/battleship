3c29385d603103170de7c7047a015fe44b9046bf
Sebastian Bischlager <sebastoan-bischlager@web.de>
2016-02-05 12:15:33 +0100

    Changes

--- GameManager/GameManager.cs
+++ GameManager/GameManager.cs
@@ -9,16 +9,68 @@ namespace GameManager
 
         int size = 10;
         int shipAmount = 5;
-        List<BattleshipKI> KIs = new List<BattleshipKI>();
+        
+        List<BattleshipKI> KIList = new List<BattleshipKI>();
 
         public Type PlayGame(Type first, Type second){
 
-            List<Ship> ships = createShips();
-            KIs.Add(KIs.NewKi(first, size));
-            KIs.Add(KIs.NewKi(first, size));
+            //Liste mit Schiffen für die beiden KIs 
+            List<Ship> shipsKI1 = createShips();
+            List<Ship> shipsKI2 = new List<Ship>(shipsKI1);
+
+            //Die KIs zu einer Liste hinzufügen
+            KIList.Add(KIs.NewKi(first, size));
+            KIList.Add(KIs.NewKi(second, size));
+
+            // Den KIs die Schiffe übergeben
+            KIList[0].SetShips(shipsKI1);
+            KIList[1].SetShips(shipsKI2);
+
+            foreach(Ship s in shipsKI1){
+
+            }
+
+           
 
 
 
+        }
+
+        private bool validatePos(Ship s){
+            bool[,] field = new bool[this.size, this.size];
+
+            if(field[s.X,s.Y]){
+                return false;
+            }
+
+            switch(s.Dir){
+                case Direction.HORIZONTAL:
+                    try{
+                        for(int i = 1; i <= s.Size; i++){
+                            bool test = field[s.X + i, s.Y];
+                            
+                            if(test){
+                                return false;
+                            }
+                        }                        
+                    }catch(IndexOutOfRangeException){
+                        return false;
+                    }
+                    break;
+                case Direction.VERTICAL:
+                    try{
+                        for(int i = 1; i <= s.Size; i++){
+                            bool test = field[s.X, s.Y + i];
+                        }
+                    }catch(IndexOutOfRangeException){
+                        return false;
+                    }
+                    break;
+            }
+
+
+            
+            return true;
         }
 
         private List<Ship> createShips(){
